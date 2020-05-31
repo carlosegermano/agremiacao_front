18bdac58b1af3bb4c839a091efc1e3b2fed5f23a
Carlos Eduardo <carlos.germano@dce.ufpb.br>
2020-05-31 11:48:48 -0300

    Desabilitando menu da tela de login

---
+++ src/pages/associados/associados.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AssociadosPage } from './associados';
+
+@NgModule({
+  declarations: [
+    AssociadosPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AssociadosPage),
+  ],
+})
+export class AssociadosPageModule {}
